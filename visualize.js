49fd8977903877f00ab8848489f42da4d4185e55
Anusree Prakash <anusree@thoughtworks.com>
2016-11-02 10:13:07 +0530

    anu: implemented sorting function

--- visualize.js
+++ visualize.js
@@ -1,21 +1,19 @@
 var data = [
-	{name:'ramesh',subject:'maths',score:87, color: 'steelblue' },
-	{name:'suresh',subject:'maths',score:45, color: 'steelblue'},
-	{name:'pokemon',subject:'english',score:65, color: 'orange'},
-	{name:'mary',subject:'kannada',score:44, color: 'green' },
-	{name:'riya',subject:'science',score:72, color: 'red'},
-	{name:'katie',subject:'social studies',score:82, color: 'darkviolet'},
-	{name:'katie',subject:'maths',score:98, color: 'steelblue'},
-	{name:'ramesh',subject:'bengali',score:25, color: 'brown'},
-	{name:'suresh',subject:'science',score:55, color: 'red'},
-	{name:'riya',subject:'tamil',score:75, color: 'magenta'},
-	{name:'pokemon',subject:'sports',score:95, color: 'grey'},
-	{name:'pokemon',subject:'social studies',score:32, color: 'darkviolet'}
+	{name:'ramesh',subject:'maths',score:87},
+	{name:'suresh',subject:'maths',score:45},
+	{name:'pokemon',subject:'english',score:65},
+	{name:'mary',subject:'kannada',score:44},
+	{name:'riya',subject:'science',score:72},
+	{name:'katie',subject:'social studies',score:82},
+	{name:'katie',subject:'maths',score:98},
+	{name:'ramesh',subject:'bengali',score:25},
+	{name:'suresh',subject:'science',score:55, },
+	{name:'riya',subject:'tamil',score:75},
+	{name:'pokemon',subject:'sports',score:95},
+	{name:'pokemon',subject:'social studies',score:32}
 ];
 
-var color = d3.scaleOrdinal()
-          .domain(['maths','english','kannada','science','social studies', 'bengali', 'tamil', 'sports'])
-          .range(['steelblue', 'orange', 'green', 'red', 'darkviolet', 'brown', 'magenta', 'grey']);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var display = function(){
   var bars = d3.select('body').select('.container')
@@ -27,27 +25,35 @@ var display = function(){
     .style('text-align', 'center')
     .style('width', function(d){return (d.score*10)+'px'})
     .classed('bars', true)
-    .style('background-color', function(d){ return d.color});
-
-    var buttonNames = ['name','subject','score'];
-
-    d3.select("body").select('.container').selectAll("input")
-      .data(buttonNames).enter().append("input")
-      .attr("type","button")
-      .attr("class","button")
-      .attr("value", function (d,i){return d;} )
-      .attr("onclick", "sortdata()");
+    .style('background-color',function(d){
+    return color(d.subject)});
 
 
+  var button = d3.select('.button-container').selectAll('button')
+    .data(Object.keys(data[0 ]))
+    .enter().append('button')
+    .attr('type','button')
+    .attr('id',function (d) {console.log(d);return d;})
+    .text(function (d) {
+      return d;
+    })
+    .attr('onclick','updateChart(this.id)');
+
+  var legend = d3.select('.legend-container').selectAll('div')
+    .data(d3.map(data, function(d){return d.subject;}).keys())
+    .enter().append('div')
+    .text(function(d){console.log(d); return d})
+    .classed('legend', true)
+    .style('background-color', function(d){
+      return color(d)
+    });
 
 }
-//
-var sortdata = function () {
-  data.sort(function(a, b) {
-      return a.score - b.score
-    });
-display();
-    }
 
+var updateChart = function(id) {
+   var bars = d3.select('.container').selectAll('.bars');
+   bars.sort((a, b) => d3.ascending(a[id], b[id]));
+
+}
 
 window.onload = display;
